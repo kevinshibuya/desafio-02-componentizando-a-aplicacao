688b833455bf0d688854e135bcf354c897e4c25e
Kevin Shibuya <kevinshibuya@hotmail.com>
2021-04-08 10:39:33 -0300

    test done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
-export function SideBar() {
+import { Button } from './Button';
+
+export function SideBar({ genres, selectedGenreId, setSelectedGenreId }: { genres: any, selectedGenreId: number, setSelectedGenreId: any }) {
   // Complete aqui
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map((genre: any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
